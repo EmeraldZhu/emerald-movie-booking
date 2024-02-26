7bc36079decf6c90bb8bc908f3e38c862552cf11
Emerald Zhu <zhuemerald@gmail.com>
2024-02-27 02:06:41 +0300

    set up vuetify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
 import './style.css'
 import App from './App.vue'
 import router from './router';
 import { pinia } from './store';
 import './firebase';
 
+const vuetify = createVuetify();
 const app = createApp(App);
+
 app.use(router);
 app.use(pinia);
+app.use(vuetify);
 
 app.mount('#app')
