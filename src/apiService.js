63652a7ba13890e4ba0e3d450d3f5abaa73992d1
Emerald Zhu <zhuemerald@gmail.com>
2024-02-27 01:42:58 +0300

    edit API key

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = 'e6b6c8af914a6106c49c3b6632be77bd';
+const API_KEY = e6b6c8af914a6106c49c3b6632be77bd;
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
