676248a2bc48d13bdf0dfb465ebeb3bdce7be5fc
Emerald Zhu <zhuemerald@gmail.com>
2024-02-27 19:07:28 +0300

    import FontAwesome

--- src/main.js
+++ src/main.js
@@ -7,11 +7,19 @@ import router from './router';
 import { pinia } from './store';
 import './firebase';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(fas);
+
 const vuetify = createVuetify();
 const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
 app.use(vuetify);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app')
